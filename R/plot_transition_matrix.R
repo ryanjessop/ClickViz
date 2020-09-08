90976a9f5d55030a4adecafc8109947d0d703846
Ryan Jessop <ryanjessop987@gmail.com>
2020-09-08 10:16:25 +0100

    first commit

---
+++ R/plot_transition_matrix.R
@@ -0,0 +1,34 @@
+#' Plot Markov chain transition matrix
+#'
+#' Builds a heatmap of the Markov chain transition matrix.
+#'
+#' @param x A number.
+#' @param y A number.
+#' @return The sum of \code{x} and \code{y}.
+#' @examples
+#' cls <- readClickstreams(file)
+#' mc <- fitMarkovChain(cls)
+#' plot_transition_matrix(mc)
+plot_transition_matrix <- function(mc){
+
+  matrix <- t(as.matrix(mc@transitions[[1]]))
+
+  plotly::plot_ly(
+    x = colnames(matrix),
+    y = colnames(matrix),
+    z = matrix,
+    colors = colorRampPalette(c("white", "yellow", "red", "dark red"))(100),
+    colorbar = list(title = 'Transition \n Probability',
+                    titlefont = list(size = 20),
+                    tickfont = list(size = 20)),
+    type = "heatmap",
+    zauto = FALSE,
+    zmin = 0,
+    zmax = max(matrix, na.rm = TRUE)) %>%
+    plotly::layout(xaxis = list(title = "To",
+                           titlefont = list(size = 30),
+                           tickfont = list(size = 20)),
+                   yaxis = list(title = "From",
+                           titlefont = list(size = 30),
+                           tickfont = list(size = 20)))
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+
+# clickviz
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of clickviz is to ...
+
+## Installation
+
+You can install the released version of clickviz from [CRAN](https://CRAN.R-project.org) with:
+
+``` r
+install.packages("clickviz")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(clickviz)
+## basic example code
+```
+
